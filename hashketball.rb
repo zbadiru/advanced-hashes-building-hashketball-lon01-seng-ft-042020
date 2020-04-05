14e5806ac1a35e215e9402b8c4b6910eac93ee6f
Zakari Badiru <zbadiru@live.co.uk>
2020-04-05 13:45:14 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -111,15 +111,15 @@ def game_hash
         :slam_dunks => 0  
         },
         {
-        :player_name => ,
-        :number => ,
-        :shoe => ,
-        :points => ,
-        :rebounds => ,
-        :assists => ,
-        :steals => ,
-        :blocks => ,
-        :slam_dunks =>  
+        :player_name => "Kemba Walker",
+        :number => 33,
+        :shoe => 15,
+        :points => 6,
+        :rebounds => 12,
+        :assists => 12,
+        :steals => 7,
+        :blocks => 5,
+        :slam_dunks => 12  
         }
       ]
     }
