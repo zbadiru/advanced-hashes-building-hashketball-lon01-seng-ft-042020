84d151b8d209a7e3ec5c6dc235a5d63d70b8409c
Zakari Badiru <zbadiru@live.co.uk>
2020-04-05 13:41:10 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -39,43 +39,43 @@ def game_hash
         :slam_dunks =>15 
         },
         {
-        :player_name => {},
-        :number => {},
-        :shoe => {},
-        :points => {},
-        :rebounds => {},
-        :assists => {},
-        :steals => {},
-        :blocks => {},
-        :slam_dunks =>{} 
+        :player_name => "Mason Plumlee",
+        :number => 1,
+        :shoe => 19,
+        :points => 26,
+        :rebounds => 11,
+        :assists => 6,
+        :steals => 3,
+        :blocks => 8,
+        :slam_dunks => 5 
         },
         {
-        :player_name => {},
-        :number => {},
-        :shoe => {},
-        :points => {},
-        :rebounds => {},
-        :assists => {},
-        :steals => {},
-        :blocks => {},
-        :slam_dunks =>{} 
+        :player_name => "Jason Terry",
+        :number => 31,
+        :shoe => 15,
+        :points => 19,
+        :rebounds => 2,
+        :assists => 2,
+        :steals => 4,
+        :blocks => 11,
+        :slam_dunks => 1 
         }
       ]
     }, 
     :away => {
-      :team_name => {}, 
-      :colors => {}, 
+      :team_name => "Charlotte Hornets", 
+      :colors => {"Turquoise", "Purple"}, 
       :players => [
         {
-        :player_name => {},
-        :number => {},
-        :shoe => {},
-        :points => {},
-        :rebounds => {},
-        :assists => {},
-        :steals => {},
-        :blocks => {},
-        :slam_dunks => {} 
+        :player_name => ,
+        :number => ,
+        :shoe => ,
+        :points => ,
+        :rebounds => ,
+        :assists => ,
+        :steals => ,
+        :blocks => ,
+        :slam_dunks =>  
         }
       ]
     }
