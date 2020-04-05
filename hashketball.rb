bc6fa41260417243b118307293dfb1943f9f960a
Zakari Badiru <zbadiru@live.co.uk>
2020-04-05 13:32:09 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -4,36 +4,36 @@ def game_hash
     :home => {
       :team_name => "Brooklyn Nets", 
       :colors => {"Black", "White"}, 
-      :players => {
-        [
+      :players => [
+        {
         :player_name => {},
-        :number => ,
-        :shoe => ,
-        :points => ,
-        :rebounds => ,
-        :assists => ,
-        :steals => ,
-        :blocks => ,
-        :slam_dunks => 
-        ]
-      }
+        :number => {},
+        :shoe => {},
+        :points => {},
+        :rebounds => {},
+        :assists => {},
+        :steals => {},
+        :blocks => {},
+        :slam_dunks =>{} 
+        }
+      ]
     }, 
     :away => {
       :team_name => {}, 
       :colors => {}, 
-      :players => {
-        [
-        :player_name => ,
-        :number => ,
-        :shoe => ,
-        :points => ,
-        :rebounds => ,
-        :assists => ,
-        :steals => ,
-        :blocks => ,
-        :slam_dunks => 
-        ]
-      }
+      :players => [
+        {
+        :player_name => {},
+        :number => {},
+        :shoe => {},
+        :points => {},
+        :rebounds => {},
+        :assists => {},
+        :steals => {},
+        :blocks => {},
+        :slam_dunks => {} 
+        }
+      ]
     }
   }
 end
