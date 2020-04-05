ed90d3aa9302673acd85fb6c610062ceeaeb39c8
Zakari Badiru <zbadiru@live.co.uk>
2020-04-05 13:29:51 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -3,7 +3,7 @@ def game_hash
   game_hash = {
     :home => {
       :team_name => "Brooklyn Nets", 
-      :colors => {Black }, 
+      :colors => {"Black", "White"}, 
       :players => {
         [
         :player_name => {},
