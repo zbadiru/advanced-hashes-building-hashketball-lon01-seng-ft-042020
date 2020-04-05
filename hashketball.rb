6bf06338a8d91b0a77ce63f3fd272d3d65a97150
Zakari Badiru <zbadiru@live.co.uk>
2020-04-05 13:20:51 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -4,21 +4,31 @@ def game_hash
     :home => {
       :team_name => {}, 
       :colors => {}, 
-      :players => {}
+      :players => {[
+        :player_name => {},
+        :number => {},
+        :shoe => {},
+        :points => {},
+        :rebounds => {},
+        :assists => {},
+        :steals => {},
+        :blocks => {},
+        :slam_dunks => {}
+        ]}
     }, 
     :away => {
       :team_name => {}, 
       :colors => {}, 
       :players => {[
-        :player_name
-        :number
-:shoe
-:points
-:rebounds
-:assists
-:steals
-:blocks
-:slam_dunks
+        :player_name => {},
+        :number => {},
+        :shoe => {},
+        :points => {},
+        :rebounds => {},
+        :assists => {},
+        :steals => {},
+        :blocks => {},
+        :slam_dunks => {}
         ]}
     }
   }
