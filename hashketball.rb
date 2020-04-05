cb67159394fdf3d9da6fccc373dcdc5a76519633
Zakari Badiru <zbadiru@live.co.uk>
2020-04-05 13:19:07 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -10,7 +10,15 @@ def game_hash
       :team_name => {}, 
       :colors => {}, 
       :players => {[
-        
+        :player_name
+        :number
+:shoe
+:points
+:rebounds
+:assists
+:steals
+:blocks
+:slam_dunks
         ]}
     }
   }
